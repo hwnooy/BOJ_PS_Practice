426ad6d0dfee9b0cbafff450fbfa224a7d654262
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2023-08-15 20:38:56 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,31 +1,22 @@
 #include <iostream>
 #include <queue>
-
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    priority_queue<int, vector<int>> pq;
-    int n;
+    priority_queue<int, vector<int>, less<>> pq;
+    int n, x;
     cin >> n;
-
-    for (int k = 0; k < n; k++) {
-        int num;
-        cin >> num;
-
-        if (pq.empty() && num==0) {
-            cout << 0 << "\n";
-        }
-
-        else if (num != 0) {
-            pq.push(num);
-        }
-
-        else if (num == 0) {
-            int max = pq.top();
-            cout << max << "\n";
-            pq.pop();
-        }
+    while(n--){
+        cin >> x;
+        
+        if (x==0){
+            if (pq.empty()) cout << 0 << "\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else if (x>=1) pq.push(x);
     }
-    return 0;
+    
 }
