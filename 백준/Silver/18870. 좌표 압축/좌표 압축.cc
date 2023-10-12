b717bdc9a271960a2d1358fb22eea7890957b941
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2023-10-12 17:00:34 +0900

    Update 좌표 압축.cc

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,23 +1,21 @@
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[1000006];
-vector<int> tmp;
-
 int main() {
-    int N; scanf("%d", &N);
-    for (int i = 0; i < N; i++) {
-        scanf("%d", arr + i);
-        tmp.push_back(arr[i]);
-    }
-    sort(tmp.begin(), tmp.end());
-    tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
-    // std::unique: 중복되는 수들을 vector의 뒤로 옮기고, 그 수들의 시작 이터레이터를 리턴
-    for (int i = 0; i < N; i++) {
-        int idx = lower_bound(tmp.begin(), tmp.end(), arr[i]) - tmp.begin();
-        printf("%d ", idx);
-    }
+	int n, arr[1000001];
+	vector<int> vec;
+	
+	cin >> n;
+	for (int k = 0; k < n; k++) {
+		cin >> arr[k];
+		vec.push_back(arr[k]);
+	}
+	sort(vec.begin(), vec.end());
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+	
+	for (int i = 0; i < n; i++) {
+		cout << lower_bound(vec.begin(), vec.end(), arr[i])-vec.begin() << " ";
+	}
+	return 0;
+
 }
