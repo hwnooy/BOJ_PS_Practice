a6fe6ff05915a79b45548c2f19c4ea7f05d3f5d9
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-09-12 18:48:52 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 2632 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sbin, bro;
+bool check[100001]; 
+
+int bfs() {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // 우선순위 큐 (시간, 위치)
+
+    // 시작 위치 체크
+    fill(check, check + 100001, false); 
+    check[sbin] = true;
+    pq.push({ 0, sbin });  
+
+    while (!pq.empty()) {
+        int time = pq.top().first;
+        int n = pq.top().second;
+        pq.pop();
+        
+        if (n == bro) {
+            return time; 
+        }
+
+        if (n * 2 < 100001 && !check[n * 2]) {
+            check[n * 2] = true;
+            pq.push({ time, n * 2 });
+        }
+
+        if (n + 1 < 100001 && !check[n + 1]) {
+            check[n + 1] = true;
+            pq.push({ time + 1, n + 1 });
+        }
+
+        if (n - 1 >= 0 && !check[n - 1]) {
+            check[n - 1] = true;
+            pq.push({ time + 1, n - 1 });
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> sbin >> bro;
+    cout << bfs();
+    
+
+    return 0;
+}
