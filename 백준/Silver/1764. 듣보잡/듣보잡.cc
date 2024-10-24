cebc314ad8a55bd5de39b701fc9e9c1ab8dfe95f
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-24 16:40:55 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 7512 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -2,35 +2,36 @@
 using namespace std;
 
 int main() {
-    int n, m, result; 
-    result = 0; 
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    unordered_set<string> notHeard;  // 첫 번째 이름 목록 저장
+    vector<string> commonNames;      // 겹치는 이름 저장
+
+    int n, m;
     cin >> n >> m;
 
-    vector<string> answer;
-    vector<string> NotHeard(n);  
-    vector<string> NotSeen(m);   
-
-    for (int i = 0; i < n; i++) {
-        cin >> NotHeard[i];
+    // 듣도 못한 사람들 입력
+    for (int k = 0; k < n; k++) {
+        string name;
+        cin >> name;
+        notHeard.insert(name);
     }
 
-    for (int i = 0; i < m; i++) {
-        cin >> NotSeen[i];
-    }
-
-    set<string> notHeardSet(NotHeard.begin(), NotHeard.end());
-    for (int i = 0; i < m; i++) {
-        if (notHeardSet.find(NotSeen[i]) != notHeardSet.end()) {
-            result++;
-            answer.push_back(NotSeen[i]);
+    // 보도 못한 사람들 입력
+    for (int k = 0; k < m; k++) {
+        string name;
+        cin >> name;
+        // 듣도 못한 사람들과 겹치는지 확인
+        if (notHeard.find(name) != notHeard.end()) {
+            commonNames.push_back(name);
         }
     }
 
-    sort(answer.begin(), answer.end());  
-    cout << result << "\n";
-
-    for (int i = 0; i < answer.size(); i++) {
-        cout << answer[i] << "\n";  
+    // 겹치는 이름의 개수와 정렬 후 출력
+    cout << commonNames.size() << "\n";
+    sort(commonNames.begin(), commonNames.end());
+    for (const string& name : commonNames) {
+        cout << name << "\n";
     }
 
     return 0;
