a020e7ce406a34347859fc342914a7b967bcde4e
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-27 18:17:49 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin >> num;
+    for (int k=0; k<num; k++){
+        int a, b; 
+        cin >> a >> b;
+        cout << "Case " << k+1 << ": " << a+b << "\n";
+    }
+}
