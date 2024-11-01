e62fd8ffa4d603da4ec6df3b2d6e6d302afc540b
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-11-01 23:43:47 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main(){
-    int n, k, result = 0;
+    int n, k, result;
+    result=0; 
     cin >> n >> k;
-    int arr[10];  
-
-    for (int i = 0; i < n; i++) {
+    int arr[11];
+    for (int i=0; i<n; i++){
        cin >> arr[i];
     }
-    
-    for (int j = n - 1; j >= 0; j--) {
-        if (arr[j] <= k) {  
-            result += k / arr[j]; 
-            k %= arr[j];          
+    for (int j=n-1; j>=0; j--){
+        if (arr[j] > 0 && arr[j]<=k) {
+            result = result + k/arr[j];
+            k = k%arr[j];
         }
     }
-    
     cout << result; 
-    return 0;
 }
+    
